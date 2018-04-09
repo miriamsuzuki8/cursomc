0be9499585a4a1057f1c11210f5ed23c5d8ccc69
Miriam Suzuki <miriamsuzuki8@gmail.com>
2018-04-09 14:27:54 -0300

    Primeiro domínio

--- cursomc/src/main/java/com/miriamsuzuki/cursomc/resources/CategoriaResounce.java
+++ cursomc/src/main/java/com/miriamsuzuki/cursomc/resources/CategoriaResounce.java
@@ -1,15 +1,26 @@
 package com.miriamsuzuki.cursomc.resources;
 
+import java.util.ArrayList;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.miriamsuzuki.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResounce {
 
 	@RequestMapping(method=RequestMethod.GET)
-	public String listas() {
-		return "Rest funcionando";
+	public java.util.List<Categoria> listas() {
+		Categoria categoria1 = new Categoria(1, "Informática");
+		Categoria categoria2 = new Categoria(2, "Escritório");
+		
+		java.util.List<Categoria> categorias = new ArrayList<Categoria>();
+		categorias.add(categoria1);
+		categorias.add(categoria2);
+		
+		return categorias;
 	}
 }
